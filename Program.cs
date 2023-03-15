06ab81913d19e3accaa2fb7eff2e811f606e9bd9
MSRbuddy <meghanadasari222@gmail.com>
2023-03-15 17:16:20 +0530

    Address-Book System using Threads ---> UC-21 is added!

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ADBook_UC21_Using_Threads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address-Book UC-21!");
+        }
+    }
+}
